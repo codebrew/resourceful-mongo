8df207a51b40703d81b13a3f849a02800eec2e9d
Ryan Fitzgerald <ryan@codebrewstudios.com>
2011-12-28 15:10:08 -0500

    removing async bit from connection tests

--- test/mongo-connection-test.js
+++ test/mongo-connection-test.js
@@ -7,37 +7,31 @@ describe('Connecting to mongoDB', function() {
   
   before(db.start);
 
-  it("accepts a uri", function(done) {
+  it("accepts a uri", function() {
     resourceful.use("mongodb", "mongodb://test.mongodb.com:4444/resourceful-mongo-test");
     var config = resourceful.connection.config;
     
     config.host.should.equal("test.mongodb.com");
     config.port.should.equal(4444);
     config.database.should.equal("resourceful-mongo-test");
-
-    done();
   });
 
-  it("connects to default db when no config is passed in", function(done) {
+  it("connects to default db when no config is passed in", function() {
     resourceful.use("mongodb");
     var config = resourceful.connection.config;
     
     config.host.should.equal("127.0.0.1");
     config.port.should.equal(27017);
     config.database.should.equal("test");
-
-    done();
   });
 
-  it("accepts host, port and databasse", function(done) {
+  it("accepts host, port and databasse", function() {
     resourceful.use("mongodb", {host: "test.host.com", port: 5555, database : "test-db"});
     var config = resourceful.connection.config;
     
     config.host.should.equal("test.host.com");
     config.port.should.equal(5555);
     config.database.should.equal("test-db");
-
-    done();
   });
 
 });
